257291da35cde887c46a9d1d087b8f0aaf12a5a6
malmsteen08 <neziryurekli@gmail.com>
2014-09-25 14:32:56 +0300

    added MVC First App

---
+++ MVC Firs App/MVC Firs App/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Firs_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+            if (saat > 5 && saat < 11)
+            {
+                ViewBag.SaatlikDurum = "Gunaydin.";
+            }
+            else if (saat > 11 && saat < 14)
+            {
+                ViewBag.SaatlikDurum = "Oglen olmus :O";
+            }
+            else
+            {
+                ViewBag.SaatlikDurum = "Iyi Aksamlar.";
+            }
+            return View();
+        }
+
+    }
+}
